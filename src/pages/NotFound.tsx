ccff042faaf45b0e45a2f3978518e964b1ac11f7
Asilbek Egamnazarov <asilbekegamnazarov999@gmail.com>
2026-01-29 23:39:13 -0500

    fix: replace video with image for 404 error page and update styles

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,25 +1,14 @@
-import error404 from "../assets/404-error.mp4";
+import error404 from "../assets/404-error.svg";
 import RouterLink from "../hooks/RouterLink";
 
 const NotFound = () => {
   return (
-    <div
-      className="h-screen flex flex-col justify-center items-center bg-black text-white text-center p-5"
-    >
-      <video
-        autoPlay
-        muted
-        playsInline
-        height="auto"
-        style={{ maxWidth: "90%", borderRadius: "12px" }}
-      >
-        <source src={error404} type="video/mp4" />
-        Brauzeringiz video ni qo'llab-quvvatlamaydi.
-      </video>
+    <div className="h-screen flex flex-col justify-center items-center bg-black text-white text-center p-5">
+      <img className="w-full h-3/4" src={error404} alt="404 error logo" />
 
       <RouterLink
         to="/"
-        className="py-3.5 px-10 bg=[#ff3366] border text-white rounded-[50px] no-underline text-[1.2rem] font-bold mt-6"
+        className="py-3.5 px-10 bg-white border text-black rounded-[50px] no-underline text-[1.2rem] font-bold mt-6"
       >
         Bosh sahifaga qaytish
       </RouterLink>
